7e715ef3bd830a3a5f5a75527ad8664f3a953d20
Yilmaz <yilmaz.olmez@euromsg.com>
2023-05-18 09:43:03 +0300

    Flurl Helper and Extentions added

--- Utility.Project.Core/Extensions/FlurlExtensions.cs
+++ Utility.Project.Core/Extensions/FlurlExtensions.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Flurl.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,86 @@ namespace Utility.Project.Core.Extensions
 {
     public static class FlurlExtensions
     {
+
+        public static IFlurlRequest ToFlurlRequest(this string baseUrl, int timeoutSeconds = 30)
+        {
+            IFlurlRequest result = baseUrl.AllowAnyHttpStatus()
+                .WithTimeout(timeoutSeconds);
+            return result;
+        }
+
+        public static HttpResponseMessage Get(this IFlurlRequest request)
+        {
+            var task = Task.Run(async () => await request.GetAsync());
+
+            HttpResponseMessage response = task.Result?.ResponseMessage;
+
+            return response;
+        }
+
+        public static HttpResponseMessage Post(this IFlurlRequest request, object obj)
+        {
+            var task = Task.Run(async () => await request.PostJsonAsync(obj));
+
+            HttpResponseMessage response = task.Result?.ResponseMessage;
+
+            return response;
+        }
+
+        public static HttpResponseMessage Patch(this IFlurlRequest request, object obj)
+        {
+            var task = Task.Run(async () => await request.PatchJsonAsync(obj));
+
+            HttpResponseMessage response = task.Result?.ResponseMessage;
+
+            return response;
+        }
+
+        public static HttpResponseMessage Put(this IFlurlRequest request, object obj)
+        {
+            var task = Task.Run(async () => await request.PutJsonAsync(obj));
+
+            HttpResponseMessage response = task.Result?.ResponseMessage;
+
+            return response;
+        }
+
+        public static HttpResponseMessage Delete(this IFlurlRequest request)
+        {
+            var task = Task.Run(async () => await request.DeleteAsync());
+
+            HttpResponseMessage response = task.Result.ResponseMessage;
+
+            return response;
+        }
+
+
+
+
+        public static async Task<T> GetResultAsync<T>(this HttpResponseMessage response) where T : class
+        {
+            T result = await response.Content?.ReadFromJsonAsync<T>();
+
+            return result;
+        }
+
+        public static string GetResult(this HttpResponseMessage response)
+        {
+            var task = Task.Run(async () => await response.Content?.ReadAsStringAsync());
+
+            string result = task.Result;
+
+            return result;
+        }
+
+        public static T GetResult<T>(this HttpResponseMessage response) where T : class
+        {
+            var task = Task.Run(async () => await response.Content?.ReadFromJsonAsync<T>());
+
+            T result = task.Result;
+
+            return result;
+        }
+
     }
 }
--- Utility.Project.Core/Extensions/FlurlHelperExtensions.cs
+++ Utility.Project.Core/Extensions/FlurlHelperExtensions.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Flurl.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,87 @@ namespace Utility.Project.Core.Extensions
 {
     public static class FlurlHelperExtensions
     {
+        public static void Validate(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                string message = response.GetResult();
+                Exception error = new Exception(message);
+
+                throw response.StatusCode switch
+                {
+                    HttpStatusCode.BadRequest => new ApplicationException(message),
+                    HttpStatusCode.NotFound => new ApplicationException("404", error),
+                    HttpStatusCode.ExpectationFailed => new ApplicationException("417", error),
+
+                    _ => new ApplicationException(response.ReasonPhrase, error),
+                };
+            }
+        }
+
+
+        public static T GetAndValidate<T>(this IFlurlRequest request) where T : class
+        {
+            HttpResponseMessage response = request.Get();
+
+            response.Validate();
+
+            T result = GetResult<T>(response);
+
+            return result;
+        }
+
+        public static T PostAndValidate<T>(this IFlurlRequest request, object obj) where T : class
+        {
+            HttpResponseMessage response = request.Post(obj);
+
+            response.Validate();
+
+            T result = GetResult<T>(response);
+
+            return result;
+        }
+
+        public static T PatchAndValidate<T>(this IFlurlRequest request, object obj) where T : class
+        {
+            HttpResponseMessage response = request.Patch(obj);
+
+            response.Validate();
+
+            T result = GetResult<T>(response);
+
+            return result;
+        }
+
+        public static T PutAndValidate<T>(this IFlurlRequest request, object obj) where T : class
+        {
+            HttpResponseMessage response = request.Put(obj);
+
+            response.Validate();
+
+            T result = GetResult<T>(response);
+
+            return result;
+        }
+
+        public static void DeleteAndValidate(this IFlurlRequest request)
+        {
+            HttpResponseMessage response = request.Delete();
+
+            response.Validate();
+        }
+
+
+
+
+        private static T GetResult<T>(HttpResponseMessage response) where T : class
+        {
+            T result;//ToDo: Farklı senaryoları dene
+            if (typeof(T) == typeof(string))
+                result = response.GetResult() as T;
+            else
+                result = response.GetResult<T>();
+            return result;
+        }
     }
 }
